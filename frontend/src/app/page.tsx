8fd18214bed72dbcc3e922eae957030d8c526ce7
Nassim Lakebir <nassim@nltech.fr>
2024-09-13 03:18:54 +0200

    [FRONTEND] Fix wind direction arrow

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
                     </span>
                     <span>
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6" style={{
-                        rotate: `-${weatherData!.wind.deg}deg`
+                        rotate: `${(weatherData!.wind.deg) - 180}deg`
                       }}>
                         <path fillRule="evenodd" d="M11.47 2.47a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06l-6.22-6.22V21a.75.75 0 0 1-1.5 0V4.81l-6.22 6.22a.75.75 0 1 1-1.06-1.06l7.5-7.5Z" clipRule="evenodd" />
                       </svg>
